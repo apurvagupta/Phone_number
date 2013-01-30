183e0dff2a1c78a1a12337ba38be63f7773fa428
apurvagupta <apurvagupta0@gmail.com>
2013-01-30 17:21:02 +0530

    third commit after error message by apurva

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe Phone do
-  it "should have 10 digits" do
-    phone = Phone.new :number => "9888276765"
-    phone.save.should == true
+  before :all do
+    phone = Phone.new
   end
+  it "should not be blank" do
+    phone.number = ""
+    phone.save.should == false
+    phone.errors[:number].first.should == "can't be blank"
+  end
+  it "should contain 10 digits" do
+       phone.number = "28888"
+        phone.save.should == false
+  end
+
 end
