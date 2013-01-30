99317fb2dc1e0cd32eb1d5dd92d87071047d8d3a
Pulkit <pulkit@Event9.(none)>
2013-01-30 17:30:59 +0530

    doosra commit

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe Phone do
   before :all do
-    phone = Phone.new
+    @phone = Phone.new
   end
   it "should not be blank" do
-    phone.number = ""
-    phone.save.should == false
-    phone.errors[:number].first.should == "can't be blank"
+    @phone.number = ""
+    @phone.save.should == false
+    @phone.errors[:number].first.should == "can't be blank"
   end
   it "should contain 10 digits" do
-       phone.number = "28888"
-        phone.save.should == false
+       @phone.number = "28888"
+        @phone.save.should == false
   end
 
 end
